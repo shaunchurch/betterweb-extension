7a1f2193788a7b993fb73acae866abcc9d43dab4
Shaun Church <shaun@shaunchurch.com>
2019-04-20 09:36:52 +0100

    Use new api data shape

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 export async function loadBlocklist(): Promise<string[]> {
   const res = await fetch("https://betterweb-api.shaun.church/badnet");
-  const data = await res.json();
-  return data.records;
+  const { data } = await res.json();
+  return data;
 }
